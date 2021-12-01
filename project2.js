cc2e1c6585d53c32e843ba0702c56b4fab0590a7
Furkan ERTEN <muhammederten2001@gmail.com>
2021-12-01 12:47:57 +0000

    yeni proje eklendi

---
+++ project2.js
@@ -0,0 +1,9 @@
+for(var i=0;i<10;i++){
+    document.write(i+"<br>")
+}
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+for(var i=15;i>0;i--){
+    document.write(i+"<br>")
+}
