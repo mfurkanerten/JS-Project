d7845de52c7efe8603574f469d7d8718fd4d024d
Furkan ERTEN <muhammederten2001@gmail.com>
2021-12-01 12:43:44 +0000

    ilk commit

---
+++ gitproject.js
@@ -0,0 +1,13 @@
+var x;
+x=100;
+document.write(x);
+x++;
+document.write( "<br>" + x );
+var y,z;
+y="Furkan Erten";
+z="Sevval Bitlis";
+a = y+z;
+document.write( "<br>" + y );
+document.write("<br>"+ y + " " + z);
+document.write("<br>" + a)
+console.log(x+x); // Console.log sayfada gözükmez. İncele kısmından bak. 
